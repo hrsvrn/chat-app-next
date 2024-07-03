c95079d3bbd1eaf289ed5c23d4257640515136d7
yashsrivastava087 <135654308+yashsrivastava087@users.noreply.github.com>
2024-07-03 21:33:21 +0530

    update

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,8 +3,6 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
-import { Loader2 } from "lucide-react"
-import { Chicle } from "next/font/google"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -38,17 +36,18 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  isloading? : boolean
+  asChild?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, variant, size,isloading,  ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
     return (
-      <button className={cn(buttonVariants({variant,size,className}))} disabled={isloading} {...props}>
-          {isloading? <Loader2 className="mr-2 h-3 w-4 animate-spin"/> : null}
-          {children}
-      </button>
-      
+      <Comp
+        className={cn(buttonVariants({ variant, size, className}))}
+        ref={ref}
+        {...props}
+      />
     )
   }
 )
